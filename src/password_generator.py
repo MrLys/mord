1d4972e421fc2726ca233b92b9763e8942bf4631
Thomas F. Lyseggen <thomaslyseggen@gmail.com>
2018-08-05 21:03:28 +0200

    Fix crash when dictionary non-existant.
        When the standard dictionary found at '/usr/share/dict/words'
        doesn't exist, a backup option should be used. Decided to just
        generate a strong random password.

--- src/password_generator.py
+++ src/password_generator.py
@@ -2,16 +2,26 @@ import secrets
 import string
 import math
 def get_ascii_characters():
-    return string.ascii_lowercase + string.digits
+    return string.ascii_letters + string.digits + string.punctuation
+
 def get_words():
-    with open('/usr/share/dict/words') as f:
-        return [word.strip() for word in f]
-    
+    try:
+        with open('/usr/share/dict/words') as f:
+            return [word.strip() for word in f]
+    except FileNotFoundError efnfe:
+        return []
+
 def generate_passphrase(word_num=5):
     if word_num > 15:
         print(word_num, "words is a thad redundant.")
         word_num = 15
     words = get_words()
+    if not words:
+        print("Dictionary could not be found, will therefor not be generating",
+                "a passphrase, but a strong random password.",
+        return ''.join(secrets.choice(get_ascii_characters()) for i in
+            range(word_num*8))
+
     return ' '.join(secrets.choice(words) for i in range(word_num))
 
 def generate_passphrase_characters(min_characters=24):
@@ -21,6 +31,11 @@ def generate_passphrase_characters(min_characters=24):
         min_characters=24
     word_num = math.ceil(min_characters/9.6)
     words = get_words()
+    if not words:
+        print("Dictionary could not be found, will therefor not be generating",
+                "a passphrase, but a strong random password.",
+        return ''.join(secrets.choice(get_ascii_characters()) for i in
+            range(word_num*8))
     pharse = ' '.join(secrets.choice(words) for i in range(word_num))
     while len(phrase) < min_characters:
         phrase += secrets.choice(words)
