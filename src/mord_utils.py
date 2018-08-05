ff413befa140836a1a9cb5576c7ed48416231bc9
Thomas F. Lyseggen <thomaslyseggen@gmail.com>
2018-08-05 20:39:49 +0200

    make return value from safe_* to always be strings

--- src/mord_utils.py
+++ src/mord_utils.py
@@ -9,14 +9,14 @@ def safe_getpass(display_string):
         password = getpass(display_string + " ")
         return password
     except EOFError:
-        return -1
+        ''
 
 def safe_input(display_string):
     try:
         text = input(display_string + " ")
         return text
     except EOFError as EOF:
-        return -1 
+        ''
 
 def sensitive_on_screen(pwd):
     print(RED,"Password:",RESET,REVERSE,pwd,RESET)
@@ -26,7 +26,7 @@ def sensitive_on_screen(pwd):
     print("The data will be erased in 10 seconds")
     for remaining in range(10, 0, -1):
         sys.stdout.write("\r")
-        sys.stdout.write("{:2d} seconds remaining.".format(remaining)) 
+        sys.stdout.write("{:2d} seconds remaining.".format(remaining))
         sys.stdout.flush()
         time.sleep(1)
     print(chr(27) + "[2J") # clear screen
